df69e836127c1ebd39ad9a533d16d8bc3582575f
gustavo-it <gustavo_castrosilva@yahoo.com>
2023-01-23 11:20:28 -0300

    Configurações iniciais

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// Sempre que criarmos um componente react precisamos importar o react
+// Mesmo que você não utilize o componente dentro do arquivo.
+
+// Vamos fazer um componente sem estado, que é um componente
+// que sem estado. Ele é uma função normal que retorna um jsx.
+// jsx é igual a html, é o jsx que o react utiliza.
+
+// Para utulizar o arquivo App.css precisamos importa-lo em nosso código
+// Importar em nossos componentes
+import "./App.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hello world!</h1>
 }
 
 export default App;
